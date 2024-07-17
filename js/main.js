7991a1c96d997d31fa01b87124a18f211b1fa5bc
Brian Duncan <darksabitsuki@gmail.com>
2024-07-16 23:34:33 -0500

    Update

--- js/main.js
+++ js/main.js
@@ -12,3 +12,30 @@ tippy('#resume-download', {
     content: 'Direct Resume Download',
     placement: 'bottom'
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+function createShape(side) {
+    const shape = document.createElement('div');
+    shape.classList.add('shape');
+    shape.style.left = `${Math.random() * 10}em`;
+    shape.style.animationDuration = `${Math.random() * 5 + 5}s`;
+    
+    const shapes = ['★', '♥', '●', '■'];
+    shape.textContent = shapes[Math.floor(Math.random() * shapes.length)];
+    
+    document.querySelector(`.falling-shapes.${side}`).appendChild(shape);
+    
+    shape.addEventListener('animationend', () => shape.remove());
+  }
+  
+  setInterval(() => createShape('left'), 2000);
+  setInterval(() => createShape('right'), 2000);
