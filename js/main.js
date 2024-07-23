8de4043a68ca92c7086804e864bb23e625031d7b
Brian Duncan <darksabitsuki@gmail.com>
2024-07-23 02:41:49 -0500

    Update

--- js/main.js
+++ js/main.js
@@ -77,35 +77,35 @@ function submitEmail() {
 
     var errorFlag = 0;
 
-    if(firstName.value == "") {
+    if (firstName.value == "") {
         firstName.classList.add("error");
         errorFlag = 1;
     }
 
-    if(lastName.value == "") {
+    if (lastName.value == "") {
         lastName.classList.add("error");
         errorFlag = 1;
     }
 
-    if(email.value == "") {
+    if (email.value == "") {
         email.classList.add("error");
         errorFlag = 1;
     }
 
-    if(subject.value == "") {
+    if (subject.value == "") {
         subject.classList.add("error");
         errorFlag = 1;
     }
 
-    if(message.value == "") {
+    if (message.value == "") {
         message.classList.add("error");
         errorFlag = 1;
     }
 
-    if(errorFlag == 1) {
+    if (errorFlag == 1) {
         document.getElementById("error-text").hidden = false;
     } else {
-        
+
         $.ajax({
             method: 'POST',
             url: 'https://formsubmit.co/ajax/your@email.com',
@@ -120,7 +120,7 @@ function submitEmail() {
             },
             error: (err) => console.log(err)
         });
-        
+
     }
 }
 
@@ -178,3 +178,129 @@ function closeShowcase() {
     document.getElementById("main-page").hidden = false;
     document.getElementById("showcase-page").hidden = true;
 }
+
+// Array of your project GIFs
+const projects = [{
+        src: 'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExanZsb2s2anVscTc5ZmluNG52dWpoNG5xdWc0M3Bjemd6ZWJjcGsxNyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/ZBtlVUPFMRp35lHyyR/giphy.gif',
+        alt: 'Project 1'
+    },
+    {
+        src: 'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExbWdra2Rkc3VpcndscW9rYWQzaDJyZTlxM2Nxb2UwcTZ4ZGdueG1pOCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/f9MKnPuybaFjXW1vJD/giphy.gif',
+        alt: 'Project 2'
+    },
+    {
+        src: 'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNjBoenVsZmRlb202OWRqczBmN2w2MnhjNGthNTJzd2tjczBsaGdpdSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/H4uDu6QEjrnpdhRc96/giphy.gif',
+        alt: 'Project 3'
+    },
+    {
+        src: 'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExOHp5YndoeGNuczZjcWR0a3J3cjRwM2Rjd2tyb2V6bGpsdjFreWpuNCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/2jQz5xH7GbwFpNsusZ/giphy.gif',
+        alt: 'Project 4'
+    },
+    {
+        src: 'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExY2NqajA5MGJ5cGl4emExeW0zYmlqdXBleHU3OWhkZnI0cDU3bXN2eiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/vg0NQVelj7PIBSlqFa/giphy.gif',
+        alt: 'Project 5'
+    },
+    {
+        src: 'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHRsdDkwMWNuaDdydXF3bG94N21nZGNoaDh5eWcza2puaGtnZ2UxdyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/yjPvDsdBdvN61wTwPT/giphy.gif',
+        alt: 'Project 6'
+    },
+    {
+        src: 'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExazl6NGczN3FzbGhkZmtycWdmOTAzc2kzcHgyeDdtbWduMGsxM3RwdiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/wLKubaEL7jBukj9Iju/giphy.gif',
+        alt: 'Project 7'
+    },
+    {
+        src: 'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExa3VqZHgzM3M0ZHNxaXByd25uNG5mN3gzN3BlZmE2c2ZjNmZqOXdsdiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/KoEBCeLVvPE2sn0xCG/giphy.gif',
+        alt: 'Project 8'
+    },
+    {
+        src: 'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExa3h4Z2wwOG82Nml2N2JyOTRwMnpvOTlreTkwNDhnOGk5ZzRwMW80ZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/accifaZUcAIFSv33IZ/giphy.gif',
+        alt: 'Project 9'
+    },
+    {
+        src: 'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMTl6bWFxdGZ4MTF5bm9peHI3bzBwODRhNDYzdWg5Y3N3NmRndmV2cyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/ll6w1fbOz5CKM69OPK/giphy.gif',
+        alt: 'Project 10'
+    },
+    {
+        src: 'https://cdn.discordapp.com/attachments/855894909143351356/892021019211071549/ScreenShot00008.png?ex=66a0470f&is=669ef58f&hm=6b3f42e71197b074a7bed843fb9260b1428208ba7e4ac0ed616eb270dcda3e2d&',
+        alt: 'Project 11'
+    },
+    {
+        src: 'https://cdn.discordapp.com/attachments/855894909143351356/892021016350564352/ScreenShot00000.png?ex=66a0470f&is=669ef58f&hm=1143566352c2635e6b412ac87b8ec7e1d6602396283d73bfee47433e407cffd9&',
+        alt: 'Project 12'
+    },
+    {
+        src: 'https://cdn.discordapp.com/attachments/855894909143351356/880898438869418054/ScreenShot00013.png?ex=66a0061a&is=669eb49a&hm=59b597622f268edfabde5cd5b921d08b6f56627daeb1861abbb47f90980b3a2f&',
+        alt: 'Project 13'
+    },
+    {
+        src: 'https://cdn.discordapp.com/attachments/855894909143351356/880898437791502336/ScreenShot00007.png?ex=66a00619&is=669eb499&hm=36c7b1e127c2b2f9a39f56d5c487195cda9f08b7b7d1a4b1b7d507c1feb27521&',
+        alt: 'Project 14'
+    },
+    {
+        src: 'https://cdn.discordapp.com/attachments/1128779086500724756/1128943682821816350/giphy.gif?ex=669ffd7c&is=669eabfc&hm=7f28796515b7ba8d185809080278aa29059d5f9f13bf666aa4cd8fba98d157c6&',
+        alt: 'Project 15'
+    },
+    {
+        src: 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZWEwNzY4YzBlMTc1MmExMWE0NTFmMWQwOGJiMmNiYjA3NmQ0MGY5OSZlcD12MV9pbnRlcm5hbF9naWZzX2dpZklkJmN0PWc/LoThFNokdofTeIPRyp/giphy.gif',
+        alt: 'Project 16'
+    },
+    {
+        src: 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHp0aXM5ZGppaDQ0emU2cDFzaTc0MG04Y3V6b2x2MmdjNTFyeTU1eiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/iyeZVwRs1Sd33PrN9k/giphy.gif',
+        alt: 'Project 17'
+    },
+    {
+        src: 'https://cdn.discordapp.com/attachments/866929207153393674/883527550008164352/Untitled.png?ex=66a05c27&is=669f0aa7&hm=68083bc15d98fe74095d3b867abf680cf7c7e9c19b0135aa0bb8239a7d9a9e91&',
+        alt: 'Project 18'
+    },
+    {
+        src: 'https://cdn.discordapp.com/attachments/864174479797911573/866909852089712670/Untitled.png?ex=66a08cb7&is=669f3b37&hm=8e47a4df844310d1cb833521a4afa80c1e16a7d0ed2d4dbf6be2d26d74ba820d&',
+        alt: 'Project 19'
+    },
+];
+
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function createGridItems() {
+    const grid = document.getElementById('projectGrid');
+    shuffleArray(projects);
+
+    projects.forEach(project => {
+        const colSize = Math.floor(Math.random() * 2) + 3;
+        const gridItem = document.createElement('div');
+        gridItem.className = `col-md-${colSize} grid-item`;
+
+        // Create two image elements: one for static and one for animated
+        const staticImg = document.createElement('img');
+        staticImg.src = project.src;
+        staticImg.alt = project.alt;
+        staticImg.className = 'img-fluid static';
+
+        const animatedImg = document.createElement('img');
+        animatedImg.src = project.src;
+        animatedImg.alt = project.alt;
+        animatedImg.className = 'img-fluid animated';
+        animatedImg.style.display = 'none';
+
+        gridItem.appendChild(staticImg);
+        gridItem.appendChild(animatedImg);
+        grid.appendChild(gridItem);
+
+        // Switch between static and animated on hover
+        gridItem.addEventListener('mouseenter', function () {
+            staticImg.style.display = 'none';
+            animatedImg.style.display = 'block';
+        });
+        gridItem.addEventListener('mouseleave', function () {
+            staticImg.style.display = 'block';
+            animatedImg.style.display = 'none';
+        });
+    });
+}
+
+window.onload = createGridItems;
